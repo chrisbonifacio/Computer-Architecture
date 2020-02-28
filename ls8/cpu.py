0fe782a972c583c9464a7afdabf626c71b1f5a0e
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-02-27 22:37:56 -0500

    finished Day 4

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,11 +2,15 @@
 
 import sys
 
-# Operations
+# Operation Tables
 binary_op = {
     0b00000001: 'HLT',
     0b10000010: 'LDI',
-    0b01000111: 'PRN'
+    0b01000111: 'PRN',
+    0b01000101: 'PUSH',
+    0b01000110: 'POP',
+    0b01010000: 'CALL',
+    0b00010001: 'RET'
 }
 
 math_op = {
@@ -14,6 +18,9 @@ math_op = {
     "SUB": 0b10100001,
     "MUL": 0b10100010
 }
+
+# Global Constants
+SP = 7  # Stack Pointer
 
 
 class CPU:
@@ -24,26 +31,85 @@ class CPU:
         self.ram = [0] * 256
         # Registers
         self.reg = [0] * 8
+        self.reg[SP] = 0xF4
         self.operand_a = None
         self.operand_b = None
 
         # Internal Registers
-        self.pc = 0
-        self.mar = None
-        self.mdr = None
-        self.instructions = {}
-        self.instructions['HLT'] = self.halt
-        self.instructions['LDI'] = self.load_int
-        self.instructions['PRN'] = self.prn
+        self.pc = 0  # Program Counter
+        self.mar = None  # Memory Address Register
+        self.mdr = None  # Memory Data Register
 
-    def halt(self):
+        # Branch Table
+        self.instructions = {}
+        self.instructions['HLT'] = self.HALT
+        self.instructions['LDI'] = self.LOAD
+        self.instructions['PRN'] = self.PRINT
+        self.instructions['PUSH'] = self.PUSH
+        self.instructions['POP'] = self.POP
+        self.instructions['CALL'] = self.CALL
+        self.instructions['RET'] = self.RET
+
+    def CALL(self):
+        """Calls a subroutine (function) at the address stored in the register."""
+        self.reg[SP] -= 1
+
+        # address of the instruction
+        instruction_address = self.pc + 2
+
+        # pushing the address of the instruction onto the stack
+        self.ram[self.reg[SP]] = instruction_address
+
+        # PC is set to the address stored in the register
+        register = self.operand_a
+
+        self.pc = self.reg[register]
+
+    def RET(self):
+        self.pc = self.ram[self.reg[SP]]
+
+        self.reg[SP] += 1
+
+    def HALT(self):
+        """Exit the current program"""
         sys.exit()
 
-    def load_int(self):
+    def LOAD(self):
+        """Load value to register"""
         self.reg[self.operand_a] = self.operand_b
 
-    def prn(self):
+    def PRINT(self):
+        """Print the value in a register"""
         print(self.reg[self.operand_a])
+
+    def PUSH(self):
+        """Push the value in the given register to the top of the stack"""
+        # decrement the SP
+        global SP
+
+        self.reg[SP] -= 1
+
+        # copy the value in the given register to the address pointed to by SP
+        value = self.reg[self.operand_a]
+
+        self.ram[self.reg[SP]] = value
+
+    def POP(self):
+        """Pop the value at the top of the stack into the given register"""
+        global SP
+        # copy the value from the address pointed to by SP to the given register
+
+        # value at the address pointed to by SP
+        value = self.ram[self.reg[SP]]
+
+        # given register from argument
+        register = self.operand_a
+
+        # copying the value from memory to the given register
+        self.reg[register] = value
+
+        # increment SP
+        self.reg[SP] += 1
 
     def ram_read(self, address):
         """Accepts an address to read and returns the value stored there"""
@@ -90,7 +156,7 @@ class CPU:
             print("File not found")
             sys.exit(2)
 
-    def alu(self, op, reg_a, reg_b):
+    def ALU(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == math_op["ADD"]:
@@ -126,8 +192,8 @@ class CPU:
     def move_pc(self, IR):
         """Accepts an Instruction Register.\n
         Increments the PC by the number of arguments returned by the IR."""
-
-        self.pc += (IR >> 6) + 1
+        if (IR << 3) % 255 >> 7 != 1:
+            self.pc += (IR >> 6) + 1
 
     def run(self):
         """Run the CPU."""
@@ -143,16 +209,17 @@ class CPU:
 
             # depending on the value of the opcode, perform the actions needed for the instruction
 
-            # TODO --- if arithmathetic bit is on, run math operation
-
+            # if arithmathetic bit is on, run math operation
             if (IR << 2) % 255 >> 7 == 1:
-                self.alu(IR, self.operand_a, self.operand_b)
+                self.ALU(IR, self.operand_a, self.operand_b)
                 self.move_pc(IR)
 
+            # else, run basic operations
             elif (IR << 2) % 255 >> 7 == 0:
                 self.instructions[binary_op[IR]]()
                 self.move_pc(IR)
 
+            # if instruction is unrecognized, exit
             else:
                 print(f"I did not understand that command: {IR}")
                 print(self.trace())
